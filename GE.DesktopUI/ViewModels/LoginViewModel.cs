4fe9918d6eae9ecc859c43be21fd8b3a8dcb8182
Toms Vanders <toms.vanders@gmail.com>
2021-01-30 10:58:24 +0100

    Add Exception for LogIn Screen
    Adds exception if the login details are incorrect and displays error message on LogInScreen.

--- GE.DesktopUI/ViewModels/LoginViewModel.cs
+++ GE.DesktopUI/ViewModels/LoginViewModel.cs
@@ -42,6 +42,35 @@ namespace GE.DesktopUI.ViewModels
             }
         }
 
+        public bool IsErrorVisible
+        {
+            get 
+            {
+                bool output = false;
+
+                if (ErrorMessage?.Length > 0)
+                {
+                    output = true;
+                }
+                return output; 
+            }
+        }
+
+        private string _errorMessage;
+
+        public string ErrorMessage
+        {
+            get { return _errorMessage; }
+            set 
+            { 
+                _errorMessage = value;
+                NotifyOfPropertyChange(() => IsErrorVisible);
+                NotifyOfPropertyChange(() => ErrorMessage);
+            }
+        }
+
+
+
         public bool CanLogIn
         {
             get
@@ -62,13 +91,14 @@ namespace GE.DesktopUI.ViewModels
         {
             try
             {
+                ErrorMessage = "";
                 var result = await _apiHelper.Authenticate(UserName, Password);
 
             }
             catch (Exception ex)
             {
 
-                Console.WriteLine(ex.Message);
+                ErrorMessage = ex.Message;
             }
         }
     }
