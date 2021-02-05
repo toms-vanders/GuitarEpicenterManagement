09289106f3dff319dc4cea029e3073af449fc0ac
Toms Vanders <toms.vanders@gmail.com>
2021-02-05 16:23:04 +0100

    Update EF Database Name
    Update Database name to EFDatabase.

--- GE.WebAPI/Models/IdentityModels.cs
+++ GE.WebAPI/Models/IdentityModels.cs
@@ -21,7 +21,7 @@ namespace GE.WebAPI.Models
     public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
         public ApplicationDbContext()
-            : base("DefaultConnection", throwIfV1Schema: false)
+            : base("EFData", throwIfV1Schema: false)
         {
         }
         
